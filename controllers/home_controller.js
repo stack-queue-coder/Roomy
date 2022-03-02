9f461d5fa4676167c77cb8baec4da2d4ea94d2be
stack-queue-coder <60400251+stack-queue-coder@users.noreply.github.com>
2022-03-02 16:47:26 +0530

    comment out console.log

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 module.exports.home = (req,res)=>{
     User.count({}, function( err, count){
         if (err) throw err;
-        console.log( "Number of users:", count );
+//         console.log( "Number of users:", count );
         res.locals.cnt = count;
         return res.render('home',{
             title: 'Roomy | Home',
